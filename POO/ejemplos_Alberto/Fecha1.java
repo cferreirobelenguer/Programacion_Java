b2faee97cf05cd1aff8b633ba4d10310a751002c
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-24 10:55:08 +0100

    Create Fecha1.java

---
+++ POO/ejemplos_Alberto/Fecha1.java
@@ -0,0 +1,17 @@
+/*
+ * Ejemplo básico I de una clase  con atributos y métodos
+ */
+
+
+public class Fecha1 {
+	// Atributos 
+	public int dia;
+	public int mes;
+	public int año;
+	
+	// Métodos
+	public void imprimir() {
+		System.out.printf("%d/%d/%d", dia, mes, año);
+		}
+	
+}
