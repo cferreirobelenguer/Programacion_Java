21fceafd899a7c8508bf647c16baf40b9423b595
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-25 20:13:25 +0100

    Update and rename EstadoPizza.java to PizzaEstado.java

--- POO/pizza/EstadoPizza.java
+++
@@ -1,5 +0,0 @@
-
-public enum EstadoPizza 
-{
-	pedida,servida,encamino;
-}
---
+++ POO/pizza/PizzaEstado.java
@@ -0,0 +1,7 @@
+package pizzeria;
+
+public enum PizzaEstado {
+	//Clase especial cuyos objetos sólo pueden ser los definidos en esa clase
+	
+	pedida,entregada,servida;
+}
