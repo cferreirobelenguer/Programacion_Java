76fdca75df8ccc0de98c7758a387d8b0f51e31bf
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-25 20:15:11 +0100

    Update and rename TipoPizza.java to PizzaTipo.java

---
+++ POO/pizza/PizzaTipo.java
@@ -0,0 +1,8 @@
+
+package pizzeria;
+
+public enum PizzaTipo {
+	//Clase especial cuyos objetos sólo pueden ser los definidos en esa clase
+	margarita,cuatroquesos,barbacoa,tropical;
+
+}
--- POO/pizza/TipoPizza.java
+++
@@ -1,5 +0,0 @@
-
-public enum TipoPizza 
-{
-	MARGARITA,BARBACOA,CUATROQUESOS
-}
