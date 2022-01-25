82c87e7245397fd0199e00cd70cd21b576b6b2e1
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-25 20:09:41 +0100

    Update Pizza.java

--- POO/pizza/Pizza.java
+++ POO/pizza/Pizza.java
@@ -1,46 +1,93 @@
 
-public class Pizza 
-{
-	private static int totalpizzaspedidas=0;
-	private static int totalpizzasservidas=0;
+package pizzeria;
+
+public class Pizza {
 	
-	private TipoPizza tipo;
-	private TamañoPizza tamaño;
-	private EstadoPizza estado;
+	//Variables privadas estÃ¡ticas
+	private static int numeroPizzasPedidas;
+	private static int numeroPizzasServidas;
 	
-	public static int getTotalPedidas() 
-	{
-		return totalpizzaspedidas;
-    }
+	//Atributos privados con clase especial enum
+	private PizzaTamano tamano;
+	private PizzaTipo tipo;
+	private PizzaEstado estado;
+	private int precio;
 	
-	public static int getTotalServidas() 
-	{
-		return totalpizzasservidas;
-    }
-	
-	
-	public Pizza(TipoPizza t, TamañoPizza s) 
-	{
-		this.tipo = t;
-		this.tamaño = s;
-		this.estado = EstadoPizza.pedida;
-		totalpizzaspedidas++;
+	public Pizza(PizzaTipo tipo,PizzaTamano tamano,int precio) {
+		//Constructor con parÃ¡metros tipo y tamano
+		this.tamano=tamano;
+		this.tipo=tipo;
+		//Por defecto se fila el estado como pedido
+		this.estado=PizzaEstado.pedida;
+		//Se incrementan las pizzas pedidas cuando se crea una pizza
+		numeroPizzasPedidas++;
+		this.precio=precio;
 	}
 	
-	public void sirve() 
-	{
-		if(estado == EstadoPizza.pedida) 
-		{
-		this.estado = EstadoPizza.servida;
-		totalpizzasservidas++;
-		}
+	//Getter y Setter
+	public PizzaTamano getTamano() {
+		return tamano;
+	}
+
+	public void setTamano(PizzaTamano tamano) {
+		this.tamano = tamano;
+	}
+
+	public PizzaTipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(PizzaTipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public PizzaEstado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(PizzaEstado estado) {
+		this.estado = estado;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	
+	//MÃ©todos estÃ¡ticos para poder ver las variables estÃ¡ticas privadas numeroPizzasPedidas, numeroPizzasServidas
+	public static int getTotalPedidas() {
+		return numeroPizzasPedidas;
+	}
+	public static int getTotalServidas() {
+		return numeroPizzasServidas;
 	}
 
 	@Override
-	public String toString() 
-	{
-		return "Pizza [tipo=" + tipo + ", tamaño=" + tamaño + ", estado=" + estado + "]";
+	//toString()
+	public String toString() {
+		return "Pizza [Tamano=" + tamano + ", Tipo=" + tipo + ", Estado=" + estado + "Precio= "+precio+"â‚¬ ]";
 	}
+	
+	//MÃ©todo servir, se cambia el estado a servido y se incrementan las pizzas servidas
+	public void sirve() {
+		if(estado==PizzaEstado.pedida) {
+		numeroPizzasServidas++;
+		//Cambia el estado a servido
+		this.estado=PizzaEstado.servida;
+		}
+	}
+	//FunciÃ³n de la superclase que compara objetos en funciÃ³n del precio
+		public int compareTo(Pizza o) {
+			int resultado=0;
+			if(this.precio<o.precio){
+				resultado=-1;
+			}else if(this.precio>o.precio){
+				resultado=1;
+			}
+		return resultado;
+		}
 	
 	
 }
