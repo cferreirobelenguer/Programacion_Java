3b6b40e27bbd49ddf9e40991f945769ab4872db3
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-25 20:14:10 +0100

    Update and rename TamañoPizza.java to Pizzatamano.java

---
+++ POO/pizza/Pizzatamano.java
@@ -0,0 +1,9 @@
+
+package pizzeria;
+
+//Clase especial cuyos objetos sólo pueden ser los definidos en esa clase
+public enum PizzaTamano {
+	
+	pequeña,mediana,familiar;
+
+}
--- POO/pizza/TamañoPizza.java
+++
@@ -1,5 +0,0 @@
-
-public enum TamañoPizza 
-{
-NORMAL,MEDIANA,FAMILIAR
-}
