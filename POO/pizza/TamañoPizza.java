dde60cfdf98840537cdf0e0d37bbd44656aa6488
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-25 17:38:38 +0100

    Add files via upload

---
+++ POO/pizza/TamañoPizza.java
@@ -0,0 +1,5 @@
+
+public enum TamañoPizza 
+{
+NORMAL,MEDIANA,FAMILIAR
+}
---
+++ POO/pizza/TestPizza.java
@@ -0,0 +1,20 @@
+
+public class TestPizza
+{
+    public static void main ( String argv[])
+    {
+        
+    Pizza p1 = new Pizza(TipoPizza.MARGARITA,   TamañoPizza.MEDIANA);
+    Pizza p2 = new Pizza(TipoPizza.CUATROQUESOS,TamañoPizza.FAMILIAR);
+    p2.sirve();
+    Pizza p3 = new Pizza(TipoPizza.BARBACOA,TamañoPizza.NORMAL);
+    System.out.println("INFORMACION SOBRE LAS PIZZAS");
+    System.out.println(p1);
+    System.out.println(p2);
+    System.out.println(p3);
+    p1.sirve();
+    System.out.println(" Total Pizza pedidas: "  + Pizza.getTotalPedidas());
+    System.out.println(" Total Pizza servidas: " + Pizza.getTotalServidas());
+    
+    }
+}
