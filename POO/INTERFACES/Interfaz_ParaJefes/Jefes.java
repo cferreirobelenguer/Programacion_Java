33d39d767cc944a02cc6528763a28376138302f7
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-29 21:07:51 +0100

    Create Jefes.java

---
+++ POO/INTERFACES/Interfaz_ParaJefes/Jefes.java
@@ -0,0 +1,34 @@
+
+package cargoJefes;
+
+import java.util.Date;
+
+//Esta clase hereda de Empleados e implementa una interfaz para definir el cargo del jefe
+public class Jefes extends Empleados implements Parajefes{
+	//Atributo de la clase Jefes
+	private double incentivo;
+	private String cargo;
+	public Jefes(String nombre, Date fechaAlta, double sueldo, double incentivo) {
+		//Constructor que hereda de Empleados los atributos
+		super(nombre,fechaAlta,sueldo);
+		this.incentivo=incentivo;
+	}
+	
+	//Se llama a getDescripcion para describir al empleado
+	public String getDescripcion() {
+		return "El empleado Jefe cuyo nombre es "+this.getNombre()+" cuya fecha de alta fue el "+this.getFechaAlta()+" y cuyo sueldo es "+this.getSueldo()+"€ con un incentivo de "+this.incentivo+"€ ,tiene un cargo de ";
+		
+	}
+
+	@Override
+	public void setCargo(String cargo) {
+		this.cargo=cargo;
+		
+	}
+
+	@Override
+	public String getCargo() {
+		return cargo;
+	}
+
+}
