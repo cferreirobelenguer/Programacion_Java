667dda6fd07b62f90e8e28158bfb2b32fc188fad
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-01-29 23:52:59 +0100

    Create ParaTrabajadores.java

---
+++ POO/INTERFACES/Interfaz_ParaJefes/ParaTrabajadores.java
@@ -0,0 +1,11 @@
+
+
+package cargoJefes;
+//Se sobreentiende que es public abstract se puede poner o no
+public interface ParaTrabajadores {
+	//método abstracto
+	double setBonus(double gratificacion);
+	//constante
+	public final static double bonus=200;
+	//Se puede poner double bonus=200 sin el public static final
+}
