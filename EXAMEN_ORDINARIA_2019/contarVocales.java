df8d94c82fc3aa31297ea8a43aa8b13348ee8466
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-02 19:26:22 +0100

    Update contarVocales.java

--- EXAMEN_ORDINARIA_2019/contarVocales.java
+++ EXAMEN_ORDINARIA_2019/contarVocales.java
@@ -22,30 +22,30 @@ public class contarVocales {
 	static Scanner sc=new Scanner(System.in);
 	
 	public static void contar(ArrayList lista) {
+		//Variable que contiene las vocales
 		String vocal="aeiou";
 		int contador;
 		for(int i=0;i<lista.size();i++) {
 			contador=0;
+			//Se recorre el arrayList y se cogen los datos con casting de String
 			String palabra=(String) lista.get(i);
 			System.out.println(palabra);
+			//Se recorre la cadena y cada char se pasa a String
 			for(int j=0;j<palabra.length();j++) {
 				String caracter=String.valueOf(palabra.charAt(j));
+				//Si vocal contiene el caracter se incrementa el contador
 				if(vocal.indexOf(caracter)!=-1){
-				int num=vocal.indexOf(palabra);
-				//System.out.println(num);
 				contador++;
 				
 				}
 			}
-			
-			
 			System.out.println("Número de vocales: "+contador);
 		}
 		
 		
 	}
 	public static void main(String[] args) {
-		
+		//Se crea ArrayList y se añaden datos
 		ArrayList<String>lista=new ArrayList<String>();
 		lista.add("holaa a a a");
 		lista.add("hola mundo holita holaaaa");
