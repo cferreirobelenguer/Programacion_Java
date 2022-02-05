7f28df604e9a8b25b5bc4d3315ad6b8363383f88
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2022-02-05 20:34:46 +0100

    Update sumaMedia.java

--- FICHEROS/sumaMedia.java
+++ FICHEROS/sumaMedia.java
@@ -40,6 +40,23 @@ public class sumaMedia {
 		System.out.println("Suma: "+suma);
 		System.out.println("Media: "+media);
 	}
+	
+	/*
+	*Este ejercicio también se puede usar usando la clase Scanner mediante BufferedReader (que lee por líneas y no por caracteres);
+	BufferedReader=null;
+	try{
+		in=new BufferedReader(new FileReader("C:/Users/Carol/Downloads/programa.txt"));
+		String texto= in.readLine(); //leemos la cadena con los números
+		String[] subcadenas=texto.split(" ");
+		double suma=0;
+		for(int i=0;i<subcadenas.length;i++){
+			suma+=Double.valueOf(subcadenas[i]);  //Las convertimos y acumulamos
+		}
+		System.out.println("suma: "+suma+"\tmedia: "+suma/subcadenas.length);
+		}catch (IOException ex){
+			System.out.println(ex.Message());
+		}
+	*/
 	public static void main(String[] args) throws IOException {
 		
 		FileReader fichero=new FileReader("C:/Users/Carol/Downloads/programa.txt");
